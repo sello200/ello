8b29280f6296a9f5c35030b276c6f3368b9ca594
sello200 <carlossello20@gmail.com>
2026-03-23 01:45:15 +0700

    Add function to calculate average of three numbers

---
+++ FungsiRataRata.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float rataRata(float a, float b, float c) {
+    return (a + b + c) / 3;
+}
+
+int main() {
+    float x, y, z, hasil;
+
+    printf("Masukkan tiga angka: ");
+    scanf("%f %f %f", &x, &y, &z);
+
+    hasil = rataRata(x, y, z);
+
+    printf("Rata-rata = %.2f\n", hasil);
+
+    return 0;
+}
